0b72488fb4831994288927f61eb73fc4dbc9b12f
Daniyal Saleem <daniyalsaleemqureshi@gmail.com>
2024-11-09 18:58:45 +0500

    signup_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_8/login_page.dart';
+import 'package:flutter_application_8/signup_page.dart';
 
 main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const LoginPage(),
+      home: const Signup(),
       theme: ThemeData(fontFamily: "Google"),
     );
   }
---
+++ lib/signup_page.dart
@@ -0,0 +1,191 @@
+import 'package:flutter/material.dart';
+
+class Signup extends StatelessWidget {
+  const Signup({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 30),
+        child: Column(
+          children: [
+            const Center(
+              child: Text(
+                "Signup",
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Row(
+              children: [
+                Text(
+                  "Username",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.person_2_outlined,
+                    color: Colors.grey,
+                  ),
+                  hintText: "Type your username",
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Row(
+              children: [
+                Text(
+                  "Password",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.lock_outlined,
+                    color: Colors.grey,
+                  ),
+                  hintText: "Type your password",
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  "Forgot Password?",
+                  // style: TextStyle(fontWeight: FontWeight.w100),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  gradient: const LinearGradient(
+                      colors: [Colors.cyan, Colors.purple]),
+                  borderRadius: const BorderRadius.all(Radius.circular(25)),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.cyanAccent,
+                        spreadRadius: 0.3,
+                        blurRadius: 14
+                        // spreadRadius: 1,
+                        // blurStyle: BlurStyle.normal,
+                        )
+                  ],
+                  border: Border.all(color: Colors.cyan)),
+              child: const Center(
+                child: Text(
+                  "LOGIN",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w100, color: Colors.white),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Or Sign Up Using",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                      color: Color(0xff3C5A98)),
+                  child: Image.asset("logo/facebook.png"),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                      color: Color(0xff509EF3)),
+                  child: Image.asset("logo/twitter.png"),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(215)),
+                      color: Color(0xffDF482D)),
+                  child: const Center(
+                      child: Text(
+                    "G",
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontFamily: "Google"),
+                  )),
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 70,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Have not account yet?",
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "SIGN UP",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
